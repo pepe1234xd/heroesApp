c60a129fe0238eb0863ef6f8f16c4c2b6f92bc3c
Jose Alfredo <joseafedox@gmail.com>
2022-12-28 17:39:06 -0600

    creating modules and components

---
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent {
+
+}
