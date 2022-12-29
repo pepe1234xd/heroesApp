b547a443f5e052869b8c1c5ad40b9ece2bace908
Jose Alfredo <joseafedox@gmail.com>
2022-12-29 11:42:05 -0600

    activating buttons

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent {
+export class HeroeComponent implements OnInit {
+
+/**
+ *
+ */
+  constructor(private activatedRoute:ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({id})=> console.log(id));
+  }
 
 }
