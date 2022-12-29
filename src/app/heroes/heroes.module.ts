d1fc494337bb569ba0d4100f978f46c3c1bc2092
Jose Alfredo <joseafedox@gmail.com>
2022-12-29 11:31:05 -0600

    creating heroes-tarjeta component

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -8,6 +8,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../material/material.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { MaterialModule } from '../material/material.module';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
+    HeroeTarjetaComponent,
+    ImagenPipe
   ],
   imports: [
     CommonModule,
